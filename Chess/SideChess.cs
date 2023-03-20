71158aee9e17ba21cd14fa62e1e43ea10012eeae
Комаров Дмитрий <105514231+Myrmlok@users.noreply.github.com>
2023-03-20 18:39:44 +0300

    Update SideChess.cs
    changed

--- Chess/SideChess.cs
+++ Chess/SideChess.cs
@@ -7,7 +7,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows;
-
+using System.IO;
 namespace MVMM
 {
     public class SideChess
@@ -34,17 +34,17 @@ namespace MVMM
             FiguresMany = new ObservableCollection<Figures>();
             for(int i = 0; i < 8; i++)
             {
-                FiguresMany.Add(new Pawn { X=i, Y=Y_pawns,IsWhite=isWhite,SourceImage = $@"C:\Users\User\Desktop\Шаг\Дмитрий\C#\homework\Chess\Chess\img\{"pawn"+src+".png"}"});
+                FiguresMany.Add(new Pawn { X=i, Y=Y_pawns,IsWhite=isWhite,SourceImage = $@"{Directory.GetCurrentDirectory()}\img\{"pawn"+src+".png"}"});
             }
-            FiguresMany.Add(new Rook { X = 7, Y = Y, IsWhite = isWhite, SourceImage = $@"C:\Users\User\Desktop\Шаг\Дмитрий\C#\homework\Chess\Chess\img\{"Rook" + src + ".png"}" });
-            FiguresMany.Add(new Rook { X = 0, Y = Y, IsWhite = isWhite, SourceImage = $@"C:\Users\User\Desktop\Шаг\Дмитрий\C#\homework\Chess\Chess\img\{"Rook" + src + ".png"}" });
-            FiguresMany.Add(new Horse { X = 1, Y = Y,IsWhite=isWhite, SourceImage = $@"C:\Users\User\Desktop\Шаг\Дмитрий\C#\homework\Chess\Chess\img\{"Horse" + src + ".png"}" });
-            FiguresMany.Add(new Horse { X = 6, Y = Y,IsWhite=isWhite, SourceImage = $@"C:\Users\User\Desktop\Шаг\Дмитрий\C#\homework\Chess\Chess\img\{"Horse" + src + ".png"}" });
+            FiguresMany.Add(new Rook { X = 7, Y = Y, IsWhite = isWhite, SourceImage = $@"{Directory.GetCurrentDirectory()}\img\{"Rook" + src + ".png"}" });
+            FiguresMany.Add(new Rook { X = 0, Y = Y, IsWhite = isWhite, SourceImage = $@"{Directory.GetCurrentDirectory()}\img\{"Rook" + src + ".png"}" });
+            FiguresMany.Add(new Horse { X = 1, Y = Y,IsWhite=isWhite, SourceImage = $@"{Directory.GetCurrentDirectory()}\img\{"Horse" + src + ".png"}" });
+            FiguresMany.Add(new Horse { X = 6, Y = Y,IsWhite=isWhite, SourceImage = $@"{Directory.GetCurrentDirectory()}\img\{"Horse" + src + ".png"}" });
            
-            FiguresMany.Add(new Elephant { X = 2, Y = Y, IsWhite = isWhite, SourceImage = $@"C:\Users\User\Desktop\Шаг\Дмитрий\C#\homework\Chess\Chess\img\{"Elephant" + src + ".png"}" });
-            FiguresMany.Add(new Elephant { X = 5, Y = Y, IsWhite = isWhite, SourceImage = $@"C:\Users\User\Desktop\Шаг\Дмитрий\C#\homework\Chess\Chess\img\{"Elephant" + src + ".png"}" });
-            FiguresMany.Add(new Queen { X=3,Y=Y,IsWhite = isWhite, SourceImage = $@"C:\Users\User\Desktop\Шаг\Дмитрий\C#\homework\Chess\Chess\img\{"Queen" + src + ".png"}" });
-            king = new King { X = 4, Y = Y, IsWhite = isWhite, SourceImage = $@"C:\Users\User\Desktop\Шаг\Дмитрий\C#\homework\Chess\Chess\img\{"King" + src + ".png"}" };
+            FiguresMany.Add(new Elephant { X = 2, Y = Y, IsWhite = isWhite, SourceImage = $@"{Directory.GetCurrentDirectory()}\img\{"Elephant" + src + ".png"}" });
+            FiguresMany.Add(new Elephant { X = 5, Y = Y, IsWhite = isWhite, SourceImage = $@"{Directory.GetCurrentDirectory()}\img\{"Elephant" + src + ".png"}" });
+            FiguresMany.Add(new Queen { X=3,Y=Y,IsWhite = isWhite, SourceImage = $@"{Directory.GetCurrentDirectory()}\img\{"Queen" + src + ".png"}" });
+            king = new King { X = 4, Y = Y, IsWhite = isWhite, SourceImage = $@"{Directory.GetCurrentDirectory()}\img\{"King" + src + ".png"}" };
             FiguresMany.Add(king);
         }
         public event PropertyChangedEventHandler PropertyChanged;
